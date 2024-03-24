8868d1b832f569b699a85ef4bf6e15e1387ca4cb
Nicholas Folis <heffawoozle77@gmail.com>
2024-03-24 13:26:50 -0500

    Modified and completed 'act2_task1.rb'.

---
+++ act2_task1.rb
@@ -0,0 +1,8 @@
+def extract_hashtags(text)
+    pattern = /#.[a-z]+/
+    hashtags = text.scan(pattern)
+    hashtags.flatten
+  end
+  
+  puts extract_hashtags("Love this #beautiful day! #sunny")
+  
